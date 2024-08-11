e6d99d2d58f560fd6d5647ede05e893734ec507c
Pico Geyer <picogeyer@gmail.com>
2024-08-11 19:26:36 +0200

    Day 3 part 2 completed

--- 2023/zig/3/calc.zig
+++ 2023/zig/3/calc.zig
@@ -37,7 +37,7 @@ const DigitStrIterator = struct {
     }
 };
 
-/// Return a slice of strings
+/// Return a slice of strings owned by the caller
 fn readInput(in_file: std.fs.File, allocator: Allocator) ![][]u8 {
     const reader = in_file.reader();
     var list = std.ArrayList([]u8).init(allocator);
@@ -57,53 +57,66 @@ fn findNextDigitStr(line: []const u8) DigitStrIterator {
     return .{ .index = 0, .line = line };
 }
 
-fn isSymbol(char: u8) bool {
-    for (SymbolChars) |s| {
+fn inSlice(char: u8, symbol_chars: []const u8) ?u8 {
+    for (symbol_chars) |s| {
         if (char == s) {
-            return true;
+            return s;
         }
     }
-    return false;
+    return null;
 }
+
+const SymbolPos = struct {
+    symbol: u8,
+    line: usize,
+    offset: usize,
+};
 
 /// Checks whether the string specified is adjacent to a symbol character
 /// Adjacent can be diagonal as well
-fn isPartNum(strings: []const []const u8, lineNo: usize, offsetStart: usize, offsetEnd: usize) bool {
+fn findPartNum(
+    strings: []const []const u8,
+    lineNo: usize,
+    offsetStart: usize,
+    offsetEnd: usize,
+    symbols: []const u8,
+) ?SymbolPos {
     for (offsetStart..offsetEnd + 1) |offset| {
         // Special casing for start and end since we have to check diagonals and
         // an extra column
         if (offset == offsetStart) {
             // Could be improved, too much repitition
-            // left upper diag
             if (offset > 0) {
-                if (isSymbol(strings[lineNo][offset - 1])) {
-                    return true;
+                if (inSlice(strings[lineNo][offset - 1], symbols)) |s| {
+                    return .{ .symbol = s, .line = lineNo, .offset = offset - 1 };
                 }
+                // left upper diag
                 if (lineNo > 0) {
-                    if (isSymbol(strings[lineNo - 1][offset - 1])) {
-                        return true;
+                    if (inSlice(strings[lineNo - 1][offset - 1], symbols)) |s| {
+                        return .{ .symbol = s, .line = lineNo - 1, .offset = offset - 1 };
                     }
                 }
+                // left lower diag
                 if (lineNo + 1 < strings.len) {
-                    if (isSymbol(strings[lineNo + 1][offset - 1])) {
-                        return true;
+                    if (inSlice(strings[lineNo + 1][offset - 1], symbols)) |s| {
+                        return .{ .symbol = s, .line = lineNo + 1, .offset = offset - 1 };
                     }
                 }
             }
         }
         if (offset == offsetEnd) {
             if (offset + 1 < strings[lineNo].len) {
-                if (isSymbol(strings[lineNo][offset + 1])) {
-                    return true;
+                if (inSlice(strings[lineNo][offset + 1], symbols)) |s| {
+                    return .{ .symbol = s, .line = lineNo, .offset = offset + 1 };
                 }
                 if (lineNo > 0) {
-                    if (isSymbol(strings[lineNo - 1][offset + 1])) {
-                        return true;
+                    if (inSlice(strings[lineNo - 1][offset + 1], symbols)) |s| {
+                        return .{ .symbol = s, .line = lineNo - 1, .offset = offset + 1 };
                     }
                 }
                 if (lineNo + 1 < strings.len) {
-                    if (isSymbol(strings[lineNo + 1][offset + 1])) {
-                        return true;
+                    if (inSlice(strings[lineNo + 1][offset + 1], symbols)) |s| {
+                        return .{ .symbol = s, .line = lineNo + 1, .offset = offset + 1 };
                     }
                 }
             }
@@ -111,20 +124,46 @@ fn isPartNum(strings: []const []const u8, lineNo: usize, offsetStart: usize, off
 
         // Above and below cases
         if (lineNo > 0) {
-            if (isSymbol(strings[lineNo - 1][offset])) {
-                return true;
+            if (inSlice(strings[lineNo - 1][offset], symbols)) |s| {
+                return .{ .symbol = s, .line = lineNo - 1, .offset = offset };
             }
         }
         if (lineNo + 1 < strings.len) {
-            if (isSymbol(strings[lineNo + 1][offset])) {
-                return true;
+            if (inSlice(strings[lineNo + 1][offset], symbols)) |s| {
+                return .{ .symbol = s, .line = lineNo + 1, .offset = offset };
             }
         }
     }
-    return false;
+    return null;
+}
+
+const GearPart = std.meta.Tuple(&.{ []const u8, SymbolPos });
+const GearPair = struct {
+    symbol: SymbolPos,
+    part1: []const u8,
+    part2: []const u8,
+};
+
+// Takes a list of potential gears and returns a list of gears that have
+// 2 part numbers connected to it
+fn findGears(allocator: Allocator, potential_gears: std.ArrayList(GearPart)) ![]GearPair {
+    var foundGears = std.ArrayList(GearPair).init(allocator);
+    for (potential_gears.items, 0..) |g, i| {
+        const gear_pos = g[1];
+        // Search the rest of the list for a matching gear
+        for (i + 1..potential_gears.items.len) |j| {
+            const m = potential_gears.items[j];
+            const other_pos = m[1];
+            if (std.meta.eql(gear_pos, other_pos)) {
+                try foundGears.append(.{ .symbol = gear_pos, .part1 = g[0], .part2 = m[0] });
+            }
+        }
+    }
+    return try foundGears.toOwnedSlice();
 }
 
 pub fn main() !void {
+    //const in_fn = "i2.txt";
     const in_fn = "input.txt";
     const in_file = try std.fs.cwd().openFile(in_fn, .{});
     defer in_file.close();
@@ -140,21 +179,42 @@ pub fn main() !void {
         allocator.free(strings);
     }
     var sum_of_parts: u32 = 0;
+    var sum_of_gear_ratios: u32 = 0;
+    var potential_gears = std.ArrayList(GearPart).init(allocator);
+    defer potential_gears.deinit();
     for (strings, 0..) |line, ln| {
         var digitsItr = findNextDigitStr(line);
         while (digitsItr.next()) |di| {
-            const is_part = isPartNum(strings, ln, di.start, di.end);
+            const symbol_found = findPartNum(strings, ln, di.start, di.end, SymbolChars);
+            const gear_found = findPartNum(strings, ln, di.start, di.end, "*");
             const part_num_str = strings[ln][di.start .. di.end + 1];
             const part_num = try std.fmt.parseInt(u32, part_num_str, 10);
-            if (is_part) {
+            if (gear_found) |g| {
+                std.debug.print("Gear symbol found at Ln {d} , offset: {d}\n", .{ g.line, g.offset });
+                try potential_gears.append(.{ part_num_str, g });
+            }
+            if (symbol_found) |_| {
+                //std.debug.print("{d}: {d} Start index: {d}, end index {d}\n", .{ ln, part_num, di.start, di.end });
                 //std.debug.print("Is a part number\n", .{});
                 sum_of_parts += part_num;
             } else {
-                std.debug.print("{d}: {d} Start index: {d}, end index {d}\n", .{ ln, part_num, di.start, di.end });
-                std.debug.print("Is not a part number\n", .{});
+                //std.debug.print("Is not a part number\n", .{});
             }
         }
         std.debug.print("\n", .{});
     }
     std.debug.print("Sum of all parts is: {d}\n", .{sum_of_parts});
+    const gear_pairs = try findGears(allocator, potential_gears);
+    defer allocator.free(gear_pairs);
+    for (gear_pairs) |g| {
+        std.debug.print(
+            "Found a gear pair at ln {d}, offset: {d} Part 1: {s}, Part 2: {s}\n",
+            .{ g.symbol.line, g.symbol.offset, g.part1, g.part2 },
+        );
+        const ratio1: u32 = try std.fmt.parseInt(u32, g.part1, 10);
+        const ratio2: u32 = try std.fmt.parseInt(u32, g.part2, 10);
+        const gear_ratio = ratio1 * ratio2;
+        sum_of_gear_ratios += gear_ratio;
+    }
+    std.debug.print("Sum of gear ratios is {d}\n", .{sum_of_gear_ratios});
 }
